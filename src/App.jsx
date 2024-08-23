9edd0d113cc995d0812baf239a71d4473fac6a4c
LTDieu <lethuandieu@gmail.com>
2024-08-23 19:29:18 +0700

    vd27

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
+// () => { }
+const App = () => {
   const [count, setCount] = useState(0)
-
+  // function myFunction() {
+  //   console.log(">>> Runnnnn")
+  // }
+  const myFunction = () => {
+    console.log(">>> abc")
+  }
+  myFunction();
   return (
     <>
       <div>
