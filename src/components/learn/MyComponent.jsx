3432828b83756833f103239ed464c84b36d1cb10
LTDieu <lethuandieu@gmail.com>
2024-08-23 20:00:38 +0700

    vd29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,7 @@
+//component = html + css + js
+const MyComponent = () => {
+    return (
+        <div> LTD  </div>
+    );
+}
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,12 @@
+const SecondComponent = () => {
+    return (
+        <div> second component</div>
+    );
+}
+const ThirdComponent = () => {
+    return (
+        <div> third component</div>
+    );
+}
+export { ThirdComponent };
+export { SecondComponent };
